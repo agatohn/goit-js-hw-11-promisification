985e6aa53f8f679eacaa2cb438aca94c0aee5153
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-09-02 19:27:10 +0300

    updated task2

--- js/task2.js
+++ js/task2.js
@@ -5,12 +5,14 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
+const toggleUserState = (allUsers, userName) => {
   const updatedUsers = allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user,
   );
-
-  callback(updatedUsers);
+  const promise = new Promise(resolve => {
+    resolve(updatedUsers);
+  });
+  return promise;
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
@@ -18,8 +20,8 @@ const logger = updatedUsers => console.table(updatedUsers);
 /*
  * Сейчас работает так
  */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
 
 /*
  * Должно работать так
